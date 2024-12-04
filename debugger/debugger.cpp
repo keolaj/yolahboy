f51b6ec7ee3f64dc88f925634dd5021a72563013
Keola <keolajarvegren@gmail.com>
2024-12-04 15:46:16 -0800

    removed files

--- debugger/debugger.cpp
+++ debugger/debugger.cpp
@@ -21,7 +21,7 @@ extern "C" {
 #include "imgui.h"
 #include "./backends/imgui_impl_sdl3.h"
 #include "./backends/imgui_impl_sdlrenderer3.h"
-#include "../imgui_memory_editor.h"
+#include "./imgui_memory_editor.h"
 #include "./imgui_custom_widgets.h"
 
 typedef struct {
--- emulator_main.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "core/global_definitions.h"
-
-int run_emulator(LPVOID t_args);
